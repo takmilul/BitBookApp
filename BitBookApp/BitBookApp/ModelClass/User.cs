701f98d134d26fc3a36131da693b5002fd2994f9
takmilul <tonmoy4222@gmail.com>
2016-11-16 00:24:52 +0600

    UserBasicInfo model class created

---
+++ BitBookApp/BitBookApp/ModelClass/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BitBookApp.ModelClass
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Gender { get; set; }
+
+        public User(int id, string firstName, string lastName, string email, string password, string dateOfBirth, string gender)
+            : this(firstName, lastName, email, password, dateOfBirth, gender)
+        {
+            Id = id;
+        }
+
+        public User(string firstName, string lastName, string email, string password, string dateOfBirth, string gender)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+            DateOfBirth = dateOfBirth;
+            Gender = gender;
+        }
+    }
+}
---
+++ BitBookApp/BitBookApp/ModelClass/UsersBasicInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BitBookApp.ModelClass
+{
+    public class UsersBasicInfo
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Location { get; set; }
+        public string Education { get; set; }
+        public string Experience { get; set; }
+        public string Gender { get; set; }
+        public string ProPicPath { get; set; }
+        public string CoverPicPath { get; set; }
+    }
+}
